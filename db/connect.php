a83d57ffd2817aabd27aec5a3241b846870cd358
Lukas Grasse <lukasgrasse@gmail.com>
2015-03-04 21:06:36 -0700

    fix

--- db/connect.php
+++ db/connect.php
@@ -1,6 +1,6 @@
 <?php
-	
-	require_once(ROOT.'/db/config.php');
+
+	require_once('./config.php');
 
 	mysql_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASS)
 	or die("<p>Connection error".mysql_error()."</p>");
