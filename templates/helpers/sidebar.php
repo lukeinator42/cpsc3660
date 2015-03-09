8e07ddf581331ecf063c3b3a5dda97fdcc3713e6
root <root@ip-172-31-47-75.us-west-2.compute.internal>
2015-03-09 21:47:15 +0000

    fixed sidebarissue

--- templates/helpers/sidebar.php
+++ templates/helpers/sidebar.php
@@ -7,27 +7,27 @@
       <a href="?action=index">Overview</a>
       </li>
 
-      <li <?php if( $_GET['action']==='pos' ) echo 'class="active"'?> >
+      <li <?php if( isset($_GET['action'])&&$_GET['action']==='pos' ) echo 'class="active"'?> >
       <a href="?action=pos">Point-of-Sale</a>
       </li>
 
-      <li <?php if( $_GET['action']==='products' ) echo 'class="active"'?> >
+      <li <?php if( isset($_GET['action'])&&$_GET['action']==='products' ) echo 'class="active"'?> >
       <a href="?action=products">Products</a>
       </li>
 
-      <li <?php if( $_GET['action']==='invoices' ) echo 'class="active"'?> >
+      <li <?php if( isset($_GET['action'])&&$_GET['action']==='invoices' ) echo 'class="active"'?> >
       <a href="?action=invoices">Invoices</a>
       </li>
 
-      <li <?php if( $_GET['action']==='purchase_orders' ) echo 'class="active"'?> >
+      <li <?php if( isset($_GET['action'])&&$_GET['action']==='purchase_orders' ) echo 'class="active"'?> >
       <a href="?action=purchase_orders">Purchase Orders</a>
       </li>
 
-      <li <?php if( $_GET['action']==='customers' ) echo 'class="active"'?> >
+      <li <?php if( isset($_GET['action'])&&$_GET['action']==='customers' ) echo 'class="active"'?> >
       <a href="?action=customers">Customers</a>
       </li>
 
-      <li <?php if( $_GET['action']==='employees' ) echo 'class="active"'?> >
+      <li <?php if( isset($_GET['action'])&&$_GET['action']==='employees' ) echo 'class="active"'?> >
       <a href="?action=employees">Employees</a>
       </li>
 
