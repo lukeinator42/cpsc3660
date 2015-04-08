bf49520d608a3a6c5a7587f9fb979b1d5ded7251
michael-wilson1993 <wilsonm_1993@hotmail.com>
2015-04-08 09:08:58 -0800

    added changes to templates

--- templates/customers.php
+++ templates/customers.php
@@ -1,4 +1,14 @@
 
+<?php
+//database connection
+require_once(ROOT.'/db/connect.php'); 
+
+//query to get data for products
+$sql_query = 
+"SELECT * from CUSTOMER";
+$result = mysql_query($sql_query);
+
+?>
 
 
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
@@ -11,127 +21,90 @@
       <thead>
         <tr>
           <th>#</th>
-          <th>Header</th>
-          <th>Header</th>
-          <th>Header</th>
-          <th>Header</th>
+          <th>Name</th>
+          <th>street Address</th>
+          <th>Billing Address</th>
+          <th>Date of Birth</th>
+          <th>Phone #</th>
+          <th>Email</th>
+          <th>Password</th>
+          <th> credit limit</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1,001</td>
-          <td>Lorem</td>
-          <td>ipsum</td>
-          <td>dolor</td>
-          <td>sit</td>
-        </tr>
-        <tr>
-          <td>1,002</td>
-          <td>amet</td>
-          <td>consectetur</td>
-          <td>adipiscing</td>
-          <td>elit</td>
-        </tr>
-        <tr>
-          <td>1,003</td>
-          <td>Integer</td>
-          <td>nec</td>
-          <td>odio</td>
-          <td>Praesent</td>
-        </tr>
-        <tr>
-          <td>1,003</td>
-          <td>libero</td>
-          <td>Sed</td>
-          <td>cursus</td>
-          <td>ante</td>
-        </tr>
-        <tr>
-          <td>1,004</td>
-          <td>dapibus</td>
-          <td>diam</td>
-          <td>Sed</td>
-          <td>nisi</td>
-        </tr>
-        <tr>
-          <td>1,005</td>
-          <td>Nulla</td>
-          <td>quis</td>
-          <td>sem</td>
-          <td>at</td>
-        </tr>
-        <tr>
-          <td>1,006</td>
-          <td>nibh</td>
-          <td>elementum</td>
-          <td>imperdiet</td>
-          <td>Duis</td>
-        </tr>
-        <tr>
-          <td>1,007</td>
-          <td>sagittis</td>
-          <td>ipsum</td>
-          <td>Praesent</td>
-          <td>mauris</td>
-        </tr>
-        <tr>
-          <td>1,008</td>
-          <td>Fusce</td>
-          <td>nec</td>
-          <td>tellus</td>
-          <td>sed</td>
-        </tr>
-        <tr>
-          <td>1,009</td>
-          <td>augue</td>
-          <td>semper</td>
-          <td>porta</td>
-          <td>Mauris</td>
-        </tr>
-        <tr>
-          <td>1,010</td>
-          <td>massa</td>
-          <td>Vestibulum</td>
-          <td>lacinia</td>
-          <td>arcu</td>
-        </tr>
-        <tr>
-          <td>1,011</td>
-          <td>eget</td>
-          <td>nulla</td>
-          <td>Class</td>
-          <td>aptent</td>
-        </tr>
-        <tr>
-          <td>1,012</td>
-          <td>taciti</td>
-          <td>sociosqu</td>
-          <td>ad</td>
-          <td>litora</td>
-        </tr>
-        <tr>
-          <td>1,013</td>
-          <td>torquent</td>
-          <td>per</td>
-          <td>conubia</td>
-          <td>nostra</td>
-        </tr>
-        <tr>
-          <td>1,014</td>
-          <td>per</td>
-          <td>inceptos</td>
-          <td>himenaeos</td>
-          <td>Curabitur</td>
-        </tr>
-        <tr>
-          <td>1,015</td>
-          <td>sodales</td>
-          <td>ligula</td>
-          <td>in</td>
-          <td>libero</td>
-        </tr>
+        
+                <!--prints each row of table-->
+        <?php
+          if (!$result) {
+            echo "DB Error, could not list tables. ";
+            echo 'MySQL Error: ' . mysql_error();
+            exit;
+          }
+          //echos each row of table. 
+          //edit button links to edit form.
+          //delete button links to delete script 
+          while ($row = mysql_fetch_row($result)) {
+            echo "<tr>
+                  <td>".str_pad($row[0], 6, "0", STR_PAD_LEFT)."</td>
+                  <td>{$row[1]}</td>
+                  <td>{$row[2]}</td>
+                  <td>{$row[4]}</td>
+                  <td>{$row[5]}</td>
+                  <td>{$row[6]}</td>
+                  <td>{$row[7]}</td>
+                  <td>{$row[8]}</td>
+                  <td>{$row[9]}</td>
+         
+                  <td><a href=\"?action=edit_Customer&number={$row[0]}\" 
+                        class=\"btn btn-primary\" role=\"button\">Edit</a></td>
+
+                  <td><a href=\"scripts/php/delete_Customer.php?number={$row[0]}\" 
+                        class=\"btn btn-danger\" role=\"button\" 
+                        onclick=\"return confirm('Delete Customer?')\">Delete</a></td>
+                  </tr>";
+          }
+        ?>
+
       </tbody>
     </table>
   </div>
+    <h2 class="sub-header">Add a Customer</h2>
+  
+  <form class="col-xs-5" action="scripts/php/insert_Customer.php" method="post">
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputName" name="inputName" placeholder="Name">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputStreetAdress" name="inputStreetAdress" placeholder="Street_adress">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputBillingAddress" name="inputBillingAddress" placeholder="Billing_Address">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputDateOfBirth" name="inputDateOfBirth" placeholder="Date of Birth">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputphone" name="inputphone" placeholder="Phone Number">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputEmail" name="inputEmail" placeholder="Email">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputpassword" name="inputpassword" placeholder="Password">
+    </div>
+
+        <div class="form-group">
+        <input type="text" class="form-control" id="inputCredit" name="inputCredit" placeholder="Credit Limit">
+    </div>
+
+    <button type="submit" class="btn btn-success">Add Customer</button>
+</form>
 </div>
 
--- templates/employees.php
+++ templates/employees.php
@@ -1,8 +1,9 @@
 
 
 
+
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-  <h1 class="page-header">Employees</h1>
+  <h1 class="page-header">Customers</h1>
 
 
   <h2 class="sub-header">Section title</h2>
@@ -10,128 +11,65 @@
     <table class="table table-striped">
       <thead>
         <tr>
-          <th>#</th>
-          <th>Header</th>
-          <th>Header</th>
-          <th>Header</th>
-          <th>Header</th>
+          <th>Name</th>
+          <th>street Address</th>
+          <th>Billing Address</th>
+          <th>Date of Birth</th>
+          <th>Phone #</th>
+          <th>Email</th>
+          <th>Password</th>
+          <th>Authority level</th>
+          <th>Sales</th>
+
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1,001</td>
-          <td>Lorem</td>
-          <td>ipsum</td>
-          <td>dolor</td>
-          <td>sit</td>
-        </tr>
-        <tr>
-          <td>1,002</td>
-          <td>amet</td>
-          <td>consectetur</td>
-          <td>adipiscing</td>
-          <td>elit</td>
-        </tr>
-        <tr>
-          <td>1,003</td>
-          <td>Integer</td>
-          <td>nec</td>
-          <td>odio</td>
-          <td>Praesent</td>
-        </tr>
-        <tr>
-          <td>1,003</td>
-          <td>libero</td>
-          <td>Sed</td>
-          <td>cursus</td>
-          <td>ante</td>
-        </tr>
-        <tr>
-          <td>1,004</td>
-          <td>dapibus</td>
-          <td>diam</td>
-          <td>Sed</td>
-          <td>nisi</td>
-        </tr>
-        <tr>
-          <td>1,005</td>
-          <td>Nulla</td>
-          <td>quis</td>
-          <td>sem</td>
-          <td>at</td>
-        </tr>
-        <tr>
-          <td>1,006</td>
-          <td>nibh</td>
-          <td>elementum</td>
-          <td>imperdiet</td>
-          <td>Duis</td>
-        </tr>
-        <tr>
-          <td>1,007</td>
-          <td>sagittis</td>
-          <td>ipsum</td>
-          <td>Praesent</td>
-          <td>mauris</td>
-        </tr>
-        <tr>
-          <td>1,008</td>
-          <td>Fusce</td>
-          <td>nec</td>
-          <td>tellus</td>
-          <td>sed</td>
-        </tr>
-        <tr>
-          <td>1,009</td>
-          <td>augue</td>
-          <td>semper</td>
-          <td>porta</td>
-          <td>Mauris</td>
-        </tr>
-        <tr>
-          <td>1,010</td>
-          <td>massa</td>
-          <td>Vestibulum</td>
-          <td>lacinia</td>
-          <td>arcu</td>
-        </tr>
-        <tr>
-          <td>1,011</td>
-          <td>eget</td>
-          <td>nulla</td>
-          <td>Class</td>
-          <td>aptent</td>
-        </tr>
-        <tr>
-          <td>1,012</td>
-          <td>taciti</td>
-          <td>sociosqu</td>
-          <td>ad</td>
-          <td>litora</td>
-        </tr>
-        <tr>
-          <td>1,013</td>
-          <td>torquent</td>
-          <td>per</td>
-          <td>conubia</td>
-          <td>nostra</td>
-        </tr>
-        <tr>
-          <td>1,014</td>
-          <td>per</td>
-          <td>inceptos</td>
-          <td>himenaeos</td>
-          <td>Curabitur</td>
-        </tr>
-        <tr>
-          <td>1,015</td>
-          <td>sodales</td>
-          <td>ligula</td>
-          <td>in</td>
-          <td>libero</td>
-        </tr>
+        
+
       </tbody>
     </table>
   </div>
+    <h2 class="sub-header">Add a Customer</h2>
+  
+  <form class="col-xs-5" action="scripts/php/insert_product.php" method="post">
+
+    <div class="form-group">
+         <input type="text" class="form-control" id="inputName" name="inputName" placeholder="Name">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputStreetAdress" name="inputStreetAdress" placeholder="Street_adress">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputBillingAddress" name="inputBillingAddress" placeholder="Billing_Address">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputDateOfBirth" name="inputDateOfBirth" placeholder="Date of Birth">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputphone" name="inputphone" placeholder="Phone Number">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputEmail" name="inputEmail" placeholder="Email">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputpassword" name="inputpassword" placeholder="Password">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputAuthority" name="inputAuthority" placeholder="Authority level">
+    </div>
+
+    <div class="form-group">
+        <input type="text" class="form-control" id="inputSa" name="inputSa" placeholder="sales">
+    </div>
+
+    <button type="submit" class="btn btn-success">Add Customer</button>
+</form>
 </div>
 
